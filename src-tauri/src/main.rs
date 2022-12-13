0dd4f090fddc8f6207997c3f3b8e0119ef22ff4c
hadziqm <dimascrazz@gmail.com>
2022-12-13 17:51:57 +0700

    idk

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,7 +2,7 @@
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
-
+pub mod csv_in;
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -11,7 +11,7 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet,csv_in::testing])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
