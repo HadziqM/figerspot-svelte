35d14a82282b5ab9610d9ed967077bcf321fe030
hadziqm <dimascrazz@gmail.com>
2022-12-13 19:22:21 +0700

    little test

--- src/lib/Greet.svelte
+++ src/lib/Greet.svelte
@@ -1,12 +1,15 @@
 <script lang="ts">
   import { invoke } from "@tauri-apps/api/tauri"
+  import {open} from "@tauri-apps/api/dialog"
 
   let name = "";
   let greetMsg = ""
-
+  
   async function greet(){
+    const file = await open({filters:[{extensions:['csv'],name:"data"}]})
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsg = await invoke("greet", { name })
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    greetMsg = await invoke("testing", { path:name })
   }
 </script>
 
--- src/maincreen.svelte
+++ src/maincreen.svelte
@@ -1,13 +1,17 @@
 <script lang="ts">
-
+  import Greet from "./lib/Greet.svelte";
 </script>
 
-  <div class="black"/>
+  <div class="black">
+  <Greet/>
+  </div>
 
 <style>
   .black{
     display:flex;
     width:100vw;
     height: 100vh;
+    justify-content: center;
+    align-items: center;
   }
 </style>
