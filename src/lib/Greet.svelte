aecda02b4165debb3b9f8c5a32715c683ee84caa
hadziqm <dimascrazz@gmail.com>
2022-12-14 11:28:38 +0700

    file system

--- src/lib/Greet.svelte
+++ src/lib/Greet.svelte
@@ -7,9 +7,7 @@
   
   async function greet(){
     const file = await open({filters:[{extensions:['csv'],name:"data"}]})
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    greetMsg = await invoke("testing", { path:name })
+    greetMsg = await invoke("testing", { path:file })
   }
 </script>
 
