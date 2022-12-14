2ad82814ea0af0fcf3f32bf24c579ff80b2463b1
hadziqm <dimascrazz@gmail.com>
2022-12-14 12:34:02 +0700

    parsing

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,6 @@
 <script lang="ts">
-  import Greet from './lib/Greet.svelte'
   import Main from './maincreen.svelte'
 </script>
 
 <Main/>
-
-<style>
-  .logo.vite:hover {
-    filter: drop-shadow(0 0 2em #747bff);
-  }
-
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00);
-  }
-</style>
+  
